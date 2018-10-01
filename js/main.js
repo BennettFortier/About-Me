0f799118f8a494823215052f4cb12e6792b6980a
Ben Fortier <bennett.fortier@wustl.edu>
2018-10-01 18:10:03 -0500

    t

--- js/main.js
+++ js/main.js
@@ -85,7 +85,7 @@ $(document).ready(function() {
       //     submit.html('Sending....'); // change submit button text
       // },
       //  success: function(data) {
-      alert.html(data).fadeIn(); // fade in response data
+      //alert.html(data).fadeIn(); // fade in response data
       form.trigger('reset'); // reset form
       submit.attr("style", "display: none !important");; // reset submit button text
       // },
